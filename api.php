a137485709fd967d10dc22049e14c122539a22a7
Eric Eaglstun <postpostmodern+pinecone@gmail.com>
2014-01-08 15:12:25 -0600

    added first pass at point model/class

--- api.php
+++ api.php
@@ -8,6 +8,16 @@ namespace GeoPrecious;
 *	@return
 */
 function api( \WP_Query $wp_query ){
+	dlog( $_POST, '$_POST', 'geo-api' );
+	
+	switch( $_POST['type'] ){
+		case 'point':
+			break;
+			
+		default:
+			break;
+	}
+	
 	dbug( $wp_query->query_vars, '$wp_query' );
 	ddbug( $_POST, '$_POST' );
 }
---
+++ lib/Point.php
@@ -0,0 +1,53 @@
+<?
+
+namespace GeoPrecious;
+
+class Point{
+	private static $wpdb;
+	
+	private $acc;
+	private $lat;
+	private $lng;
+	private $stamp;
+	private $user_id;
+	
+	/*
+	*
+	*/
+	public function __construct(){
+		global $wpdb;
+		self::$wpdb = &$wpdb;
+	}
+	
+	/*
+	*
+	*/
+	public function insert(){
+		$sql = self::$wpdb->prepare( "INSERT INTO geoprecious
+									  ( user_id )
+									  VALUES
+									  ( %d )", $this->user_id );
+	}
+	
+	/*
+	*
+	*	@param float
+	*	@param float
+	*	@param int
+	*	@return
+	*/
+	public function setPoint( $lat, $lng, $acc = 0 ){
+		$this->lat = (float) $lat;
+		$this->lng = (float) $lng;
+		$this->acc = (int) $acc;
+	}
+	
+	/*
+	*
+	*	@param int
+	*	@return
+	*/
+	public function setUserID( $user_id ){
+		$this->user_id = (int) $user_id;
+	}
+}
